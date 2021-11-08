68dc24104c1ed15d335ae72245a0d75423b0c5b1
akshaylulla <48390512+akshaylulla@users.noreply.github.com>
2021-11-08 20:49:59 +0100

    add where clause

--- SQL/crossJoin.sql
+++ SQL/crossJoin.sql
@@ -1,2 +1,2 @@
-SELECT a.tech_id, a.title, b.inventor
-FROM public."Technologies" as a CROSS JOIN public."Inventor" as b;
+SELECT b.tech_id, b.title, a.inventor
+FROM public."Inventor" as a CROSS JOIN public."Technologies" as b WHERE b.tech_id=a.tech_id;
