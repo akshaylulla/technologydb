020a0efa041f6e2506cba3414a9284f2d381705d
akshaylulla <48390512+akshaylulla@users.noreply.github.com>
2021-11-08 20:58:19 +0100

    add distinct

--- SQL/contactByCategory.sql
+++ SQL/contactByCategory.sql
@@ -1,8 +1,8 @@
-SELECT email, contact_name 
+SELECT DISTINCT email, contact_name
 FROM public."Contact" as t3
 RIGHT JOIN
 (SELECT * 
-FROM public."Technology" as t1
-RIGHT JOIN public."Categories" as t2
+FROM public."Technologies" as t1
+LEFT JOIN public."Categories" as t2
 ON t1.tech_id = t2.tech_id WHERE t2.category='Analytics & Information Technology') as t4
 ON t3.email = t4.contact_email;
